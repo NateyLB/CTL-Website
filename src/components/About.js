0eb8fc2732f786a9014546d35c12543c5b353103
Nathan <nateyl.b@gmail.com>
2020-06-08 01:03:52 -0700

    updated nav bar styling, added dummy text, and added a bullet between paragraphs.

--- src/components/About.js
+++ src/components/About.js
@@ -10,40 +10,49 @@ const About = props =>{
                 with the customer, from design to fabric, we are with you evey step of the way. Please take a look some of CausingTheLost's work and contact with 
                 inquiries.
             </p>
-            <p> CausingTheLost is an online avant-garde custom embroidery boutique by Nicholas White. Full custom embroidery can be done on an array of 
-                garments suchs as pants, shirts, jackets, with an emphasis on hats. Hats can be sourced in-house or be provided by you. Custom work is done end-to-end
-                with the customer, from design to fabric, we are with you evey step of the way. Please take a look some of CausingTheLost's work and contact with 
-                inquiries.
+            <img className='bullet' src={require('../resources/favicon_io_transparent/favicon.ico')} alt="CausingTheLost.logo"></img>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <p> CausingTheLost is an online avant-garde custom embroidery boutique by Nicholas White. Full custom embroidery can be done on an array of 
-                garments suchs as pants, shirts, jackets, with an emphasis on hats. Hats can be sourced in-house or be provided by you. Custom work is done end-to-end
-                with the customer, from design to fabric, we are with you evey step of the way. Please take a look some of CausingTheLost's work and contact with 
-                inquiries.
+            <img className='bullet' src={require('../resources/favicon_io_transparent/favicon.ico')} alt="CausingTheLost.logo"></img>
+            <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et 
+                quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores 
+                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius 
+                modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, 
+                nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem 
+                eum fugiat quo voluptas nulla pariatur?
             </p>
-            <p> CausingTheLost is an online avant-garde custom embroidery boutique by Nicholas White. Full custom embroidery can be done on an array of 
-                garments suchs as pants, shirts, jackets, with an emphasis on hats. Hats can be sourced in-house or be provided by you. Custom work is done end-to-end
-                with the customer, from design to fabric, we are with you evey step of the way. Please take a look some of CausingTheLost's work and contact with 
-                inquiries.
+            <img className='bullet' src={require('../resources/favicon_io_transparent/favicon.ico')} alt="CausingTheLost.logo"></img>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <p> CausingTheLost is an online avant-garde custom embroidery boutique by Nicholas White. Full custom embroidery can be done on an array of 
-                garments suchs as pants, shirts, jackets, with an emphasis on hats. Hats can be sourced in-house or be provided by you. Custom work is done end-to-end
-                with the customer, from design to fabric, we are with you evey step of the way. Please take a look some of CausingTheLost's work and contact with 
-                inquiries.
+            <img className='bullet' src={require('../resources/favicon_io_transparent/favicon.ico')} alt="CausingTheLost.logo"></img>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <p> CausingTheLost is an online avant-garde custom embroidery boutique by Nicholas White. Full custom embroidery can be done on an array of 
-                garments suchs as pants, shirts, jackets, with an emphasis on hats. Hats can be sourced in-house or be provided by you. Custom work is done end-to-end
-                with the customer, from design to fabric, we are with you evey step of the way. Please take a look some of CausingTheLost's work and contact with 
-                inquiries.
+            <img className='bullet' src={require('../resources/favicon_io_transparent/favicon.ico')} alt="CausingTheLost.logo"></img>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <p> CausingTheLost is an online avant-garde custom embroidery boutique by Nicholas White. Full custom embroidery can be done on an array of 
-                garments suchs as pants, shirts, jackets, with an emphasis on hats. Hats can be sourced in-house or be provided by you. Custom work is done end-to-end
-                with the customer, from design to fabric, we are with you evey step of the way. Please take a look some of CausingTheLost's work and contact with 
-                inquiries.
+            <img className='bullet' src={require('../resources/favicon_io_transparent/favicon.ico')} alt="CausingTheLost.logo"></img>
+            <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et 
+                quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores 
+                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius 
+                modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, 
+                nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem 
+                eum fugiat quo voluptas nulla pariatur?
             </p>
-            <p> CausingTheLost is an online avant-garde custom embroidery boutique by Nicholas White. Full custom embroidery can be done on an array of 
-                garments suchs as pants, shirts, jackets, with an emphasis on hats. Hats can be sourced in-house or be provided by you. Custom work is done end-to-end
-                with the customer, from design to fabric, we are with you evey step of the way. Please take a look some of CausingTheLost's work and contact with 
-                inquiries.
+            <img className='bullet' src={require('../resources/favicon_io_transparent/favicon.ico')} alt="CausingTheLost.logo"></img>
+            <p> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint 
+                occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et 
+                expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas 
+                assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint 
+                et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus 
+                asperiores repellat.
             </p>
 
         </section>
