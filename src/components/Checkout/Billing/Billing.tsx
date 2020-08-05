3748a0807d81f4ee3508cafdfe19a84b7c5f0faf
Nathan <nateyl.b@gmail.com>
2020-08-05 02:01:09 -0700

    stripe payment page set up

--- src/components/Checkout/Billing/Billing.tsx
+++ src/components/Checkout/Billing/Billing.tsx
@@ -1,12 +1,112 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useHistory } from 'react-router'
+import { connect } from 'react-redux'; 
+import {
+  CardElement,
+  useStripe,
+  useElements
+} from "@stripe/react-stripe-js";
+
+import { axiosBase } from '../../../utils/axiosBase'
 
 const Billing = props => {
-
-    return (
-        <div>
-            <p>BILLING</p>
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState('');
+  const stripe = useStripe();
+  const elements = useElements();
+  const match = useHistory();
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+      axiosBase()
+      .post('/payment/create-payment-intent', props.cart.cart )
+      .then(res => {
+        return res;
+      })
+      .then(data => {
+        setClientSecret(data.data.clientSecret);
+      });
+  }, []);
+  const cardStyle = {
+    style: {
+      base: {
+        color: "#32325d",
+        fontFamily: `'Tenali Ramakrishna', sans-serif`,
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d"
+        }
+      },
+      invalid: {
+        color: "red",
+        iconColor: "red"
+      }
+    }
+  };
+  const handleChange = async (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+  const handleSubmit = async ev => {
+    ev.preventDefault();
+    setProcessing(true);
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: ev.target.name.value
+        }
+      }
+    });
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+      match.push('/checkout/confirmation')
+    }
+  };
+  return (
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <CardElement id="card-element" options={cardStyle} onChange={handleChange} />
+      <button
+        disabled={processing || disabled || succeeded}
+        id="submit"
+      >
+        <span id="button-text">
+          {processing ? (
+            <div className="spinner" id="spinner"></div>
+          ) : (
+            "Pay"
+          )}
+        </span>
+      </button>
+      {/* Show any error that happens when processing the payment */}
+      {error && (
+        <div className="card-error" role="alert">
+          {error}
         </div>
-    )
+      )}
+      <p className={succeeded ? "result-message" : "result-message hidden"}>
+        Payment succeeded
+      </p>
+    </form>
+  );
 }
 
-export default Billing;
+const mapStateToProps = state => {
+  return {
+    cart: state.cartReducer
+  };
+};
+
+export default connect(
+mapStateToProps
+)(Billing)
--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router'
 
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import ShippingAddressForm from './ShippingAddressForm'
 import Billing from './Billing/Billing'
+
+const promise = loadStripe('pk_test_51GskCkDff7659YjxexBIJDvJWbJkniud4UAEahaunSvGhShEV3lDmtY5VXjZNa45T4UZ9T8Cdj8yhpNc5ri71Qtu003qZcgH99');
 
 
 const Checkout = () => {
@@ -14,7 +18,9 @@ const Checkout = () => {
             <ShippingAddressForm/>
             </Route>
             <Route path='/checkout/billing'>
+            <Elements stripe={ promise }>
             <Billing/>
+            </Elements>
             </Route>
         </section>
     )
